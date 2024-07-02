4786850dc653b376d1a6fd8f6ea52ddd789d0e9d
Niku <nokka200@gmail.com>
2024-07-02 17:13:59 +0300

    Muokattu index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -25,14 +26,6 @@ let notes = [
   }
 ];
 
-const generateId = () => {
-  const maxId = notes.length > 0
-  ? Math.max(...notes.map(note => Number(note.id)))
-    : 0;
-  
-  return String(maxId + 1);
-};
-
 const requestLogger = (request, response, next) => { 
   console.log('Method:', request.method);
   console.log('Path:', request.path);
@@ -58,33 +51,26 @@ app.get('/api/notes', (request, response) => {
 });
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number.parseInt(request.params.id);
-  const note = notes.find(note => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  });
 });
 
 app.post('/api/notes', (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  };
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' });
+  }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId()
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  });
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -96,7 +82,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
