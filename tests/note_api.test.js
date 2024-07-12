53fc79cfe869df672118f2d54c1abb095948a05b
Niku <nokka200@gmail.com>
2024-07-12 15:22:26 +0300

    Valmis 4b tehtäviin

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -15,16 +15,11 @@ const Note = require('../models/note')
 
 beforeEach(async () => {
   await Note.deleteMany({})
-
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-
-
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+  await Note.insertMany(helper.initialNotes)
 })
 
 test('notes are returned as json', async () => {
+  console.log('entered test')
   await api
     .get('/api/notes')
     .expect(200)
